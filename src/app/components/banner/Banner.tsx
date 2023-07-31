9a89e30afdedcdeb88fc506b3d791e4d3eecf4c1
RafaelBarbosa17 <rafaelvbarbosaa@gmail.com>
2023-07-31 19:23:55 -0300

    reformulação da página principal com melhorias no design, botão para whatsapp adicionado

--- src/app/components/banner/Banner.tsx
+++ src/app/components/banner/Banner.tsx
@@ -2,20 +2,34 @@ import Image from "next/image";
 
 const Banner = () => {
   return (
-    <section className="bg-white md:bg-[url('/banner-light.png')] dark:bg-black dark:md:bg-[url('/banner.png')] bg-cover bg-right bg-no-repeat w-full h-[90vh]">
-      <div className="md:w-2/4 w-full h-full flex justify-center items-center flex-col">
+    <section className="bg-white bg-no-repeat md:bg-right bg-center md:bg-contain bg-cover dark:bg-black w-full h-[90vh] flex flex-row" style={{backgroundImage: "url('/banner-image.png')"}}>
+      {/* elementos de estilisação */}
+      <div>
+        <Image src={'/banner-detail.png'} width={1000} height={1000} alt="vector" className="absolute h-[90vh] top-[10vh] w-[60vw] z-0 hidden md:dark:block" />
+        <Image src={'/banner-detail-light.png'} width={1000} height={1000} alt="vector" className="absolute h-[90vh] top-[10vh] w-[60vw] z-0 hidden md:block dark:hidden" />
+        <div className="absolute h-[90vh] top-[10vh] w-[100vw] bg-gradient-to-r dark:from-black from-white dark:via-black via-white via-50% to-transparent z-0 hidden">
+        </div>
+        <div className="absolute h-[90vh] top-[10vh] w-screen z-0 md:hidden block dark:bg-black bg-white dark:opacity-70 opacity-20">
+        </div>
+      </div>
+
+
+      <div className="md:w-2/4 w-full h-full pt-2 flex md:justify-start items-center gap-4 flex-col z-10">
         <Image
-            src='/logo-king-pizza.png'
-            width={300}
-            height={300}
-            alt="favicon"
-            className="block"
+          src="/logo-king-pizza.png"
+          width={500}
+          height={500}
+          alt="favicon"
+          className="block max-w-[300px]"
         />
-        <div className="md:hidden hidden min-w-[300px] max-w-[500px] min-h-[300px] bg-[url('/banner-mobile-light.png')] dark:bg-[url('/banner-mobile.png')] bg-cover bg-no-repeat bg-center"></div>
-        <h1 className="dark:text-orange-500 text-orange-800 mm:text-5xl text-4xl text-center text-bold font-allura">
+        <h1 className="dark:text-orange-500 text-4xl text-center text-bold font-allura rounded p-1">
           Direto do forno <br /> para sua casa!
         </h1>
+        <button className="p-2 rounded bg-red-700 text-white">
+          <a href="https://wa.me/5555319078278?text=Ol%C3%A1%2C+eu+quero+pizza%21" target="_blank">Pedir agora</a>
+        </button>
       </div>
+
     </section>
   );
 };
--- src/app/components/header/Theme.tsx
+++ src/app/components/header/Theme.tsx
@@ -4,7 +4,6 @@ import React, { ComponentProps, useEffect, useState } from "react";
 import { useTheme } from '@/app/context/ThemeContext';
 
 const Theme = () => {
-  const [themeValue, setThemeValue] = useState(true)
   // Usando o hook useTheme para acessar o contexto de tema
   const { theme, changeTheme } = useTheme();
 
@@ -15,19 +14,19 @@ const Theme = () => {
   })
 
   return (
-      <button 
-        data-success={themeValue}
+      <button
         onClick={() => {
           changeTheme()
-          theme === 'dark' ? setThemeValue(true) : setThemeValue(false)
         }}
-        className={`w-20 h-10 p-2 rounded-full flex items-center ${themeValue ? 'justify-end bg-white shadow-[inset_0px_2px_5px_0px_black]' : 'justify-start bg-black shadow-[inset_0px_2px_5px_0px_white]'} transition-all`}
+        className={`w-20 h-10 p-2 rounded-full flex items-center ${theme === 'dark' ? ' bg-black shadow-[inset_0px_2px_5px_0px_white]' : ' bg-white shadow-[inset_0px_2px_5px_0px_black]'} transition-all`}
       >
-        {
-            theme === 'dark'
-            ? <HiMoon className='w-8 h-8 fill-orange-500' />
-            : <HiSun className='w-8 h-8 fill-orange-500' />
-        }
+        <div className={`${theme === 'dark' ? '' : 'translate-x-full'} trnasition-all ease-linear duration-100`}>
+          {
+              theme === 'dark'
+              ? <HiMoon className='w-8 h-8 fill-orange-500' />
+              : <HiSun className='w-8 h-8 fill-orange-500' />
+          }
+        </div>
       </button>
   );
 };
