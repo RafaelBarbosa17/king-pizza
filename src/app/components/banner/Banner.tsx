d315739a18cd9dd8e47506bea089e5a4ffe64af6
RafaelBarbosa17 <rafaelvbarbosaa@gmail.com>
2023-07-31 19:30:36 -0300

    link para whatsapp funcionando

--- src/app/components/banner/Banner.tsx
+++ src/app/components/banner/Banner.tsx
@@ -5,8 +5,8 @@ const Banner = () => {
     <section className="bg-white bg-no-repeat md:bg-right bg-center md:bg-contain bg-cover dark:bg-black w-full h-[90vh] flex flex-row" style={{backgroundImage: "url('/banner-image.png')"}}>
       {/* elementos de estilisação */}
       <div>
-        <Image src={'/banner-detail.png'} width={1000} height={1000} alt="vector" className="absolute h-[90vh] top-[10vh] w-[60vw] z-0 hidden md:dark:block" />
-        <Image src={'/banner-detail-light.png'} width={1000} height={1000} alt="vector" className="absolute h-[90vh] top-[10vh] w-[60vw] z-0 hidden md:block dark:hidden" />
+        <Image src={'/banner-detail.png'} width={1000} height={1000} alt="vector" className="absolute h-[90vh] top-[10vh] w-[65vw] z-0 hidden md:dark:block" />
+        <Image src={'/banner-detail-light.png'} width={1000} height={1000} alt="vector" className="absolute h-[90vh] top-[10vh] w-[65vw] z-0 hidden md:block dark:hidden" />
         <div className="absolute h-[90vh] top-[10vh] w-[100vw] bg-gradient-to-r dark:from-black from-white dark:via-black via-white via-50% to-transparent z-0 hidden">
         </div>
         <div className="absolute h-[90vh] top-[10vh] w-screen z-0 md:hidden block dark:bg-black bg-white dark:opacity-70 opacity-20">
@@ -26,7 +26,7 @@ const Banner = () => {
           Direto do forno <br /> para sua casa!
         </h1>
         <button className="p-2 rounded bg-red-700 text-white">
-          <a href="https://wa.me/5555319078278?text=Ol%C3%A1%2C+eu+quero+pizza%21" target="_blank">Pedir agora</a>
+          <a href="https://wa.me/553190782786?text=Ol%C3%A1%2C+eu+quero+pizza%21" target="_blank">Pedir agora</a>
         </button>
       </div>
 
